30c7f1a9fed2ab1c4ad09458839ae760caab1980
Chad Tindel <chad@tindel.net>
2018-12-05 20:09:17 -0800

    now in 2 regions

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-./node_modules/serverless/bin/serverless deploy --stage dev
+#!/bin/bash
+
+export STAGE=dev
+
+./node_modules/serverless/bin/serverless deploy --stage $STAGE --region us-east-2
+./node_modules/serverless/bin/serverless deploy --stage $STAGE --region us-west-2
+
+aws dynamodb create-global-table \
+    --global-table-name phonebook-api-$STAGE \
+    --replication-group RegionName=us-east-2 RegionName=us-west-2 \
+    --region us-east-2
--- remove.sh
+++ remove.sh
@@ -1,1 +1,2 @@
-./node_modules/serverless/bin/serverless remove --stage dev
+./node_modules/serverless/bin/serverless remove --stage dev --region us-east-2
+./node_modules/serverless/bin/serverless remove --stage dev --region us-west-2
